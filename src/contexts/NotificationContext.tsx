981afefc1f1ca6b49f9451f1e6c112a84c7e620e
Roger Doré 1 <doreroger07@yahoo.fr>
2025-12-26 05:33:17 -0800

    Updated NotificationContext.tsx

--- src/contexts/NotificationContext.tsx
+++ src/contexts/NotificationContext.tsx
@@ -44,7 +44,18 @@ const NotificationContext = createContext<NotificationContextType | undefined>(u
 export const useNotifications = () => {
   const context = useContext(NotificationContext);
   if (!context) {
-    throw new Error('useNotifications must be used within a NotificationProvider');
+    return {
+      notifications: [],
+      unreadCount: 0,
+      preferences: null,
+      loading: false,
+      fetchNotifications: async () => {},
+      markAsRead: async () => {},
+      markAllAsRead: async () => {},
+      deleteNotification: async () => {},
+      updatePreferences: async () => {},
+      createNotification: async () => {}
+    };
   }
   return context;
 };
